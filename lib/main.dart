90c736d79681e8da052509317d141f9e7cd6b49d
AyatSalahEl-din <ayat.salah.eldin#gmail.com>
2025-05-26 01:27:28 +0300

    final merge.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:ieee_website/About%20Us/about.dart';
 import 'package:ieee_website/Base/base.dart';
 import 'package:ieee_website/Contact%20Us/contact.dart';
@@ -12,37 +14,26 @@ import 'package:ieee_website/Themes/my_theme.dart';
 import 'package:ieee_website/Tools&Features/tools.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:ieee_website/provider/chat_provider.dart';
+import 'package:provider/provider.dart';
 import 'chatbot/chatbot_home_screen.dart';
 import 'firebase_options.dart';
-import 'package:provider/provider.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-<<<<<<< HEAD
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
-=======
 
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await Hive.initFlutter(); // Use hive_flutter for web compatibility
   await ChatProvider.initHive(); // Make sure this is clean!
 
   runApp(
     MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ChatProvider()),
-      ],
+      providers: [ChangeNotifierProvider(create: (_) => ChatProvider())],
       child: const MyApp(),
     ),
   );
->>>>>>> 0b4e29e2b3ddc6cff32b8e0e77640c11c8ad41fd
 }
 
 class MyApp extends StatelessWidget {
@@ -70,11 +61,8 @@ class MyApp extends StatelessWidget {
             JoinUs.routeName: (context) => JoinUs(),
             Base.routeName: (context) => Base(),
             FAQ.routeName: (context) => FAQ(),
-<<<<<<< HEAD
-=======
 
             ChatbotHomeScreen.routeName: (context) => ChatbotHomeScreen(),
->>>>>>> 0b4e29e2b3ddc6cff32b8e0e77640c11c8ad41fd
           },
         );
       },
